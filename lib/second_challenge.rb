d8856315906fef0f25599cb0aff0eca0a7ce21ed
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 20:51:24 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,19 +2,18 @@ def second_challenge
   epic_tragedy = {
     montague: {
       #your key/value pairs here!
-      patriarch: {
-        attributes: {
-          name: "Lord Montague",
-          age: "53"
+      patriarch: { 
+            {
+        
         }
       },
       matriarch: {
         attributes: {
-          name: , 
+          name: ,
           age:
         }
       }
-      
+
       },
     capulet: {
       #your key/value pairs here!
